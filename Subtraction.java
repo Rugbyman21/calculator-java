b995336872a5b02c689aeef2d2664b5b7e59e8f8
Trevor Scherling <scherling.trevor@gmail.com>
2015-12-01 12:03:25 -0800

    allow user to add, subtract, multiply and divid

---
+++ Subtraction.java
@@ -0,0 +1,21 @@
+import java.io.Console;
+
+public class Subtraction {
+  public static void main(String[] args) {
+    Console myConsole = System.console();
+
+    System.out.println("Give me your first number!");
+    String stringNumber = myConsole.readLine();
+    Integer firstNumber = Integer.parseInt(stringNumber);
+
+    System.out.println("Give me your second number!");
+    String stringNum = myConsole.readLine();
+    Integer secondNumber = Integer.parseInt(stringNum);
+
+    Integer addNumber = firstNumber - secondNumber;
+
+    String stringAddNumber = Integer.toString(addNumber);
+
+    System.out.println("Subtracting the two numbers together I got: " + addNumber);
+  }
+}
